f7bcb44698e5c1d9e31d982c123558e6e313c9f9
jonatan <Jluengorequena@gmail.com>
2026-01-21 13:36:09 +0100

    fix tabla categoria

--- Migrations/20260121121926_AddUserAndOrderToCategoria.Designer.cs
+++ Migrations/20260121121926_AddUserAndOrderToCategoria.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ControlDeGastos.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ControlDeGastos.Migrations
 {
     [DbContext(typeof(ControlGastosContext))]
-    partial class ControlGastosContextModelSnapshot : ModelSnapshot
+    [Migration("20260121121926_AddUserAndOrderToCategoria")]
+    partial class AddUserAndOrderToCategoria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ Migrations/20260121121926_AddUserAndOrderToCategoria.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControlDeGastos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndOrderToCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "GastoAutomatico",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Orden",
+                table: "Categoria",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Categoria",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GastoAutomatico_UserId",
+                table: "GastoAutomatico",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categoria_UserId",
+                table: "Categoria",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categoria_User_UserId",
+                table: "Categoria",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GastoAutomatico_User_UserId",
+                table: "GastoAutomatico",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categoria_User_UserId",
+                table: "Categoria");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GastoAutomatico_User_UserId",
+                table: "GastoAutomatico");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GastoAutomatico_UserId",
+                table: "GastoAutomatico");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categoria_UserId",
+                table: "Categoria");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "GastoAutomatico");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "Categoria");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categoria");
+        }
+    }
+}
